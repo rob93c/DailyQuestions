90b4c3df231e3a283732ff2a02453186710f2517
Roberto Cella <rob.uniud@gmail.com>
2025-01-06 13:10:21 +0100

    Configure daily questions (#11)
    * customize project, add basic structure to load and search for questions
    
    * add january questions and implement logic to retrieve today's question
    
    * add february and march entries
    
    * implement custom date logic and display both buttons on the same row
    
    * add light theme support
    
    * extend light theme support to buttons

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
+import {provide, onMounted, ref} from 'vue';
 import DailyQuestion from "./components/DailyQuestion.vue";
+
+const theme = ref('dark');
+
+onMounted(() => {
+  const isDarkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  theme.value = isDarkTheme ? 'dark' : 'light';
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+    theme.value = e.matches ? 'dark' : 'light';
+  });
+});
+
+provide('theme', theme);
 </script>
 
 <template>
   <div>
     <a href="https://github.com/rob93c" target="_blank">
-      <img src="/src/assets/github.svg" class="logo github" alt="GitHub logo"/>
+      <img :src="`/src/assets/${theme}-github.svg`" :class="['logo', 'github', theme]" alt="GitHub logo"/>
     </a>
   </div>
-  <DailyQuestion />
+  <DailyQuestion/>
 </template>
 
 <style scoped>
@@ -19,7 +33,13 @@ import DailyQuestion from "./components/DailyQuestion.vue";
   transition: filter 300ms;
 }
 
-.logo.github:hover {
+/*noinspection CssUnusedSymbol*/
+.logo.github.dark:hover {
   filter: drop-shadow(0 0 2em #aeb1b1);
+}
+
+/*noinspection CssUnusedSymbol*/
+.logo.github.light:hover {
+  filter: drop-shadow(0 0 2em #000000);
 }
 </style>
--- src/components/DailyQuestion.vue
+++ src/components/DailyQuestion.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {ref} from 'vue';
+import {inject, ref} from 'vue';
 import questionsJson from './../assets/questions.json';
 
 type Questions = {
@@ -8,27 +8,42 @@ type Questions = {
   };
 };
 
+const theme = inject('theme');
+
+let loadQuestion = (date: Date) => {
+  const month = date.toLocaleString('en', {month: 'long'});
+  const day = date.getDate();
+
+  return questions[month]?.[day] || 'No question for today.';
+}
+
 const questions: Questions = questionsJson;
 const today = new Date();
-const month = today.toLocaleString('en', {month: 'long'});
-const day = today.getDate();
-const dailyQuestion = questions[month]?.[day] || 'No question for today.';
+let dailyQuestion = ref(loadQuestion(today));
 
-const customDate = ref('');
+const customDate = ref<string | null>(null);
 const handleSubmit = () => {
+  if (customDate.value) {
+    const selectedDate = new Date(customDate.value);
+    dailyQuestion.value = loadQuestion(selectedDate);
+  }
 };
 </script>
 
 <template>
   <h1>365 questions</h1>
-  <h2> {{ dailyQuestion }}</h2>
+  <h2><i> {{ dailyQuestion }} </i></h2>
+  <br>
 
   <form @submit.prevent="handleSubmit">
     <div>
       <label for="date">Scegli un giorno specifico: </label>
       <input type="date" id="date" v-model="customDate" required>
     </div>
-    <button type="submit">Carica la domanda</button>
+    <div class="button-row">
+      <button :class="['btn', theme]">Carica la domanda</button>
+      <button :class="['btn', theme]" type="button" @click="dailyQuestion = loadQuestion(today)">Oggi</button>
+    </div>
   </form>
 </template>
 
@@ -39,7 +54,25 @@ form {
   gap: 2em;
 }
 
+.button-row {
+  display: flex;
+  justify-content: center;
+  gap: 1em;
+}
+
 button {
   align-self: auto;
+}
+
+/*noinspection CssUnusedSymbol*/
+.btn.dark {
+  background-color: #333;
+  color: #fff;
+}
+
+/*noinspection CssUnusedSymbol*/
+.btn.light {
+  background-color: #c9c9c9;
+  color: #333;
 }
 </style>
