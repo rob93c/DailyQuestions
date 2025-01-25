efd62559d7ab15bb7f30f8341b32c41b25c3d7ee
Roberto Cella <rob.uniud@gmail.com>
2025-01-25 22:03:48 +0100

    move translations to json files

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import {createApp} from 'vue'
 import {createI18n} from 'vue-i18n'
 import './style.css'
 import App from './App.vue'
+import en_translations from './locales/en.json'
+import it_translations from './locales/it.json'
 
 const i18n = createI18n({
     legacy: false,
@@ -9,26 +11,10 @@ const i18n = createI18n({
     fallbackLocale: 'en',
     messages: {
         en: {
-            message: {
-                today: 'Today',
-                yesterday: 'Yesterday',
-                specificDate: 'Choose a specific date: ',
-                futureQuestion: 'Can\'t wait? You are peeking at the question for {date}:',
-                pastQuestion: 'The question for {date} was:',
-                todayQuestion: 'Buckle up! Today\'s question is:',
-                noQuestions: 'There are no questions today.'
-            }
+            message: en_translations
         },
         it: {
-            message: {
-                today: 'Oggi',
-                yesterday: 'Ieri',
-                specificDate: 'Scegli un giorno specifico: ',
-                futureQuestion: 'Impaziente? Stai sbirciando la domanda del {date}:',
-                pastQuestion: 'La domanda del {date} era:',
-                todayQuestion: 'Preparati a rispondere! La domanda di oggi è:',
-                noQuestions: 'Non ci sono domande oggi.'
-            }
+            message: it_translations
         }
     },
     datetimeFormats: {
